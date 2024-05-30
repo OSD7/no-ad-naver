ad84497c364b79a2822eae8dbfd3158bf6d61212
bum6894 <bum6894@naver.com>
2024-05-30 15:00:59 +0900

    v.1.0.2.beta

--- adblocker.py
+++ adblocker.py
@@ -10,6 +10,17 @@ def block_ads(html_content):
         r"<!--\s*ad\s*-->",
         r"class\s*=\s*['\"]?ad['\"]?",
         r"id\s*=\s*['\"]?ad['\"]?",
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:first-child > div\.wrap\.id_data > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:first-child > div\.grid1\.id_cui_data:nth-child(2)",
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:first-child > div\.wrap\.id_data > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:first-child > div\.grid1\.id_cui_data:nth-child(3)",
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:nth-child(2) > div\.wrap\.id_data > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:first-child > div\.grid1\.id_cui_data:nth-child(2)",
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:nth-child(2) > div\.wrap\.id_data > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:first-child > div\.grid1\.id_cui_data:nth-child(3)",
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:nth-child(2) > div\.wrap\.id_data > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:first-child > div\.grid1\.id_cui_data:nth-child(5)",
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:first-child > div\.wrap\.id_data > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:first-child > div\.grid1\.id_cui_data:nth-child(5)",
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:last-child > div\.wrap\.id_shopping > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:nth-child(2) > div\.grid1\.id_cui_shop_trend\.MM_REFRESH_BOX\.MM_TREND_PICK:first-child",
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:last-child > div\.wrap\.id_shopping > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:nth-child(2) > div\.grid1\.id_cui_shop_trend\.MM_REFRESH_BOX\.MM_HOTDEAL:nth-child(4)",
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:last-child > div\.wrap\.id_shopping > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:first-child > div\.grid1\.id_cui_shop_trend\.MM_REFRESH_BOX\.MM_TREND_SHOP:nth-child(3)",
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:last-child > div\.wrap\.id_shopping > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:first-child > div\.grid1\.id_cui_banner:nth-child(5)",
+        r"m\.naver\.com###_MM_REGION_TAB > div\.cp_alarm_area:last-child"
         # 광고패턴 추가 예정
     ]
 
--- server.py
+++ server.py
@@ -11,7 +11,18 @@ def block_ads(html_content):
         r"<!--\s*ad\s*-->",
         r"class\s*=\s*['\"]?ad['\"]?",
         r"id\s*=\s*['\"]?ad['\"]?",
-        # 추가적인 광고 패턴들을 여기에 추가할 수 있습니다.
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:first-child > div\.wrap\.id_data > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:first-child > div\.grid1\.id_cui_data:nth-child(2)",
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:first-child > div\.wrap\.id_data > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:first-child > div\.grid1\.id_cui_data:nth-child(3)",
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:nth-child(2) > div\.wrap\.id_data > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:first-child > div\.grid1\.id_cui_data:nth-child(2)",
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:nth-child(2) > div\.wrap\.id_data > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:first-child > div\.grid1\.id_cui_data:nth-child(3)",
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:nth-child(2) > div\.wrap\.id_data > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:first-child > div\.grid1\.id_cui_data:nth-child(5)",
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:first-child > div\.wrap\.id_data > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:first-child > div\.grid1\.id_cui_data:nth-child(5)",
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:last-child > div\.wrap\.id_shopping > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:nth-child(2) > div\.grid1\.id_cui_shop_trend\.MM_REFRESH_BOX\.MM_TREND_PICK:first-child",
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:last-child > div\.wrap\.id_shopping > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:nth-child(2) > div\.grid1\.id_cui_shop_trend\.MM_REFRESH_BOX\.MM_HOTDEAL:nth-child(4)",
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:last-child > div\.wrap\.id_shopping > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:first-child > div\.grid1\.id_cui_shop_trend\.MM_REFRESH_BOX\.MM_TREND_SHOP:nth-child(3)",
+        r"m\.naver\.com###mflick > div\.eg-flick-container > div\.flick-panel\.eg-flick-panel:last-child > div\.wrap\.id_shopping > div\.grid1_wrap\.brick-house:first-child > div\.brick-vowel:first-child > div\.grid1\.id_cui_banner:nth-child(5)",
+        r"m\.naver\.com###_MM_REGION_TAB > div\.cp_alarm_area:last-child"
+        
     ]
 
     # 정규표현식을 사용하여 광고를 찾고 제거합니다.
